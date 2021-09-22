39d31a3180273994329e840c0700bb41dc29d56e
Diórgenes Eugênio <diorgeneseugenio@gmail.com>
2021-09-22 09:53:29 -0300

    refactor: organize foldes to separete responsabilities

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,4 +1,4 @@
-const db = require('../db')
+const db = require('../../db')
 
 const { GraphQLScalarType } = require('graphql');
 const { Kind } = require('graphql/language');
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 const { ApolloServer } = require("apollo-server");
 
-const { resolvers } = require("./resolvers");
-const { typeDefs } = require("./schema");
+const { resolvers } = require("./graphql/resolvers");
+const { typeDefs } = require("./graphql/schema");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
